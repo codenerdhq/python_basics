12d215247e49466122a099177f06b2bc0015e275
CodeNerdHQ <codenerdhq@gmail.com>
2023-01-31 00:18:03 +0000

    range method

---
+++ basics/range_method.py
@@ -0,0 +1,15 @@
+# Range until 3, not included
+for i in range(3):
+    print(i)
+
+# Range from 1 to 5 (not included)
+for i in range(1, 5):
+    print(i)
+
+# Range: start 0, end 8 (not included), step of 2
+for i in range(0, 8, 2):
+    print(i)
+
+# Range in reverse, from 3 to 1
+for i in range(3, 0, -1):
+    print(i)
