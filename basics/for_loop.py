371937cba90bf6b6803286484a8edb4432cd4f89
CodeNerdHQ <codenerdhq@gmail.com>
2023-01-31 00:17:12 +0000

    added for loop file

---
+++ basics/for_loop.py
@@ -0,0 +1,10 @@
+# List Declaration in Python
+numbers = [10, 5, 12]
+
+# For loop, method 1:
+for i in numbers:
+  print(i)
+
+# For loop, method 2:
+for i in range(len(numbers)):
+    print(numbers[i])
